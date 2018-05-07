052b1944fec49896c155e034f4e5e85b50760ac9
DIRIX Pieter (s) <pieter.dirix@dt-c205-08.local>
2018-05-07 15:16:20 +0200

    Initial Commit

---
+++ werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  werkstuk1
+//
+//  Created by DIRIX Pieter (s) on 07/05/2018.
+//  Copyright © 2018 DIRIX Pieter (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
